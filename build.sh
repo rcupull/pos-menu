019f47c36ad32ce322a0f3f167b225f473a83e14
rcupull <rcupull@gmail.com>
2026-03-11 08:26:03 -0400

    add-some-changes

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# 1. Cargar NVM para que reconozca los comandos de Node
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# 2. (Opcional) Asegurarte de usar la v20.1.0
+nvm use v20.1.0 --silent
+
+
 # Nos situamos en la carpeta del script
 cd "$(dirname "$0")"
 
